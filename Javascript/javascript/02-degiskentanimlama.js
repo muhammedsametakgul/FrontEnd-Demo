4720a1b921524b0e143bd18f354731f73e93804e
Muhammed Samet Akgul <msametakgul@gmail.com>
2023-08-14 11:33:21 +0300

    Javascript added

---
+++ Javascript/javascript/02-degiskentanimlama.js
@@ -0,0 +1,28 @@
+// var değişken tanımlama
+//var serverName="kodluyoruz.org"
+//console.log(serverName)
+
+//let ile değişken tanımlama
+let serverName;
+console.log(serverName)
+
+serverName="https://kodluyoruz.org"
+console.log(serverName)
+
+/* Hatalı kullanım
+console.log(password)
+let password="1234";
+*/
+
+//let ile tanımlanan değişkenin içindeki bilgi sonradan değiştirilebilir.
+//password="sadzsda"
+
+//birleştirme / ekleme işlemleri de yapılabilir
+//password=password+"saddas"
+//password +="adssf"
+//console.log(password + "dsadas") :)
+
+
+//const değişken tanımlama
+const SERVER_PASSWORD="server1234"
+console.log(SERVER_PASSWORD)
