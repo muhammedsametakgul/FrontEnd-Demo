6ebdc30df2449e6be6c0d1d63e6b1afb4659fcd3
Muhammed Samet Akgul <msametakgul@gmail.com>
2023-08-15 12:21:33 +0300

    JS Conditional

---
+++ Javascript/javascript/if-else.js
@@ -0,0 +1,6 @@
+let username=prompt("Kullanıcı Adınızı Giriniz : ")
+if(username.length>0){
+    console.log("Giriş Başarılı")
+}else{
+    console.log("Başarısız")
+}
---
+++ Javascript/javascript/karsilastirm.js
@@ -0,0 +1,8 @@
+let price=100
+
+// == değeri eşitse
+
+console.log(price==100)
+
+// === hem değeri hem de türü eşitse
+console.log(price===100)
